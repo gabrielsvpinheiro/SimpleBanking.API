e22812f4c7277c492d6bb2b59a37cfeb560a214e
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-22 20:46:49 -0300

    feat: models implementation

---
+++ SimpleBanking.API/Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleBanking.API.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public int Balance { get; set; }
+    }
+}
---
+++ SimpleBanking.API/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleBanking.API.Models
+{
+    public class Event
+    {
+        public string Type { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Amount { get; set; }
+    }
+}

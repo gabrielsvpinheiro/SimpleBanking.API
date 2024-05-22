61afc63aaf335dcc9c73685475012c733455f6a8
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-22 20:57:34 -0300

    feat: getbalance endpoint creation

---
+++ SimpleBanking.API/Controllers/BalanceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleBanking.API.Models;
+
+namespace SimpleBanking.API.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class BalanceController : ControllerBase
+    {
+        private static Dictionary<string, Account> accounts = new Dictionary<string, Account>();
+
+        [HttpGet("balance")]
+        public IActionResult GetBalance([FromQuery] string account_id) 
+        { 
+            if (accounts.ContainsKey(account_id))
+            {
+                return Ok(accounts[account_id].Balance);
+            }
+            else
+            {
+                return NotFound(0);
+            }
+        }
+    }
+}

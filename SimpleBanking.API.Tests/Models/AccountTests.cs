33e062edc9950da20efc5dc006ed6217bc6942c9
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-23 01:32:06 -0300

    test: models test creation

---
+++ SimpleBanking.API.Tests/Models/AccountTests.cs
@@ -0,0 +1,48 @@
+﻿using SimpleBanking.API.Models;
+
+namespace SimpleBanking.API.Tests.Models
+{
+    public class AccountTests
+    {
+        [Fact]
+        public void Account_ShouldHaveAnId()
+        {
+            var account = new Account();
+
+            account.Id = "12345";
+
+            Assert.NotNull(account.Id);
+            Assert.Equal("12345", account.Id);
+        }
+
+        [Fact]
+        public void Account_ShouldHaveInitialBalance()
+        {
+            var account = new Account();
+
+            account.Balance = 100;
+
+            Assert.Equal(100, account.Balance);
+        }
+
+        [Fact]
+        public void Account_ShouldUpdateBalanceCorrectly()
+        {
+            var account = new Account { Balance = 100 };
+
+            account.Balance += 50;
+
+            Assert.Equal(150, account.Balance);
+        }
+
+        [Fact]
+        public void Account_ShouldHandleNegativeBalance()
+        {
+            var account = new Account { Balance = 100 };
+
+            account.Balance -= 150;
+
+            Assert.Equal(-50, account.Balance);
+        }
+    }
+}
---
+++ SimpleBanking.API.Tests/Models/EventTests.cs
@@ -0,0 +1,60 @@
+﻿using SimpleBanking.API.Models;
+
+namespace SimpleBanking.API.Tests.Models
+{
+    public class EventTests
+    {
+        [Fact]
+        public void Event_ShouldHaveType()
+        {
+            var eventInstance = new Event();
+
+            eventInstance.Type = "transfer";
+
+            Assert.NotNull(eventInstance.Type);
+            Assert.Equal("transfer", eventInstance.Type);
+        }
+
+        [Fact]
+        public void Event_ShouldHaveOrigin()
+        {
+            var eventInstance = new Event();
+
+            eventInstance.Origin = "12345";
+
+            Assert.NotNull(eventInstance.Origin);
+            Assert.Equal("12345", eventInstance.Origin);
+        }
+
+        [Fact]
+        public void Event_ShouldHaveDestination()
+        {
+            var eventInstance = new Event();
+
+            eventInstance.Destination = "67890";
+
+            Assert.NotNull(eventInstance.Destination);
+            Assert.Equal("67890", eventInstance.Destination);
+        }
+
+        [Fact]
+        public void Event_ShouldHaveAmount()
+        {
+            var eventInstance = new Event();
+
+            eventInstance.Amount = 1000;
+
+            Assert.Equal(1000, eventInstance.Amount);
+        }
+
+        [Fact]
+        public void Event_ShouldHandleNegativeAmount()
+        {
+            var eventInstance = new Event();
+
+            eventInstance.Amount = -500;
+
+            Assert.Equal(-500, eventInstance.Amount);
+        }
+    }
+}

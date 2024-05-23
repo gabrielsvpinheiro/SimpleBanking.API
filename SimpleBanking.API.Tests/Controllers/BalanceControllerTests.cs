e1fc759268dc0565fc4a1b19e2befceee2c25abb
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-22 21:25:34 -0300

    test: new test project implemented

---
+++ SimpleBanking.API.Tests/Controllers/BalanceControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBanking.API;
+
+namespace SimpleBanking.API.Tests.Controllers
+{
+    public class BalanceControllerTests
+    {
+    }
+}

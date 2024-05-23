68ca0957e102c3fb3e896dcef662be1827736899
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-23 00:48:24 -0300

    fix: accounts dictionary configuration

--- SimpleBanking.API.Tests/Controllers/BalanceControllerTests.cs
+++ SimpleBanking.API.Tests/Controllers/BalanceControllerTests.cs
@@ -7,24 +7,23 @@ namespace SimpleBanking.API.Tests.Controllers
 {
     public class BalanceControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
-        private readonly BalanceController _client;
+        private readonly BalanceController _controller;
 
-        public BalanceControllerTests() 
+        public BalanceControllerTests()
         {
-            _client = new BalanceController();
+            var accounts = new Dictionary<string, Account>
+            {
+                { "1234", new Account { Id = "1234", Balance = 100 } }
+            };
+            _controller = new BalanceController(accounts);
         }
 
         [Fact]
         public void ShouldReturnOkWithCorrectBalanceWhenAccountExists()
         {
             var account_id = "1234";
-            var accounts = new Dictionary<string, Account>
-            {
-                { account_id, new Account { Balance = 100 } }
-            };
-            _client.SetAccounts(accounts);
 
-            var result = _client.GetBalance(account_id);
+            var result = _controller.GetBalance(account_id);
 
             var okResult = Assert.IsType<OkObjectResult>(result);
             Assert.Equal(100, okResult.Value);
@@ -34,10 +33,8 @@ namespace SimpleBanking.API.Tests.Controllers
         public void ShouldReturnNotFoundWhenAccountDoesNotExist()
         {
             var account_id = "456";
-            var accounts = new Dictionary<string, Account>();
-            _client.SetAccounts(accounts);
 
-            var result = _client.GetBalance(account_id);
+            var result = _controller.GetBalance(account_id);
 
             Assert.IsType<NotFoundObjectResult>(result);
         }

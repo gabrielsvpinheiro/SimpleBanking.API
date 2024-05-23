79452400888509f7a68371400216dc5406cfe9da
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-23 20:19:25 -0300

    feat: making models optional

--- SimpleBanking.API/Models/Event.cs
+++ SimpleBanking.API/Models/Event.cs
@@ -3,8 +3,8 @@
     public class Event
     {
         public string Type { get; set; }
-        public string Origin { get; set; }
-        public string Destination { get; set; }
+        public string? Origin { get; set; }
+        public string? Destination { get; set; }
         public int Amount { get; set; }
     }
 }

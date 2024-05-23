cd6f500479004e537a21579357b736c12d1b6f21
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-22 21:38:22 -0300

    test: httpClient configuration

--- SimpleBanking.API.Tests/Controllers/BalanceControllerTests.cs
+++ SimpleBanking.API.Tests/Controllers/BalanceControllerTests.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using SimpleBanking.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace SimpleBanking.API.Tests.Controllers
 {
-    public class BalanceControllerTests
+    public class BalanceControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _client;
+
+        public BalanceControllerTests(WebApplicationFactory<Program> factory) 
+        {
+            _client = factory.CreateClient();
+        }
     }
 }

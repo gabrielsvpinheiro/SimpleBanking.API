115ffafdce988fa439326914daf2fec8c2a52b9f
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-23 17:22:30 -0300

    feat: new environment configurations

--- SimpleBanking.API/Program.cs
+++ SimpleBanking.API/Program.cs
@@ -7,9 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    var kestrelSection = context.Configuration.GetSection("Kestrel");
+    options.Configure(kestrelSection);
+});
+
 var app = builder.Build();
 
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
